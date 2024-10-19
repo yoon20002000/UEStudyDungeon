f391935a93c31f2e0e02e6fd5a6677e6f4478824
yoon20002000 <yoon20002000@naver.com>
2024-10-19 22:26:48 +0900

    Init 7

---
+++ Source/CryptRaider/Grabber.cpp
@@ -0,0 +1,95 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Grabber.h"
+#include "Engine/World.h"
+#include "DrawDebugHelpers.h"
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	UPhysicsHandleComponent* PhysicsHandler = GetPhysicsHandle();;
+	if(PhysicsHandler == nullptr)
+	{
+		return;
+	}
+
+	FVector TargetLocation = GetComponentLocation() + GetForwardVector() * HoldDistance;
+	PhysicsHandler->SetTargetLocationAndRotation(TargetLocation, GetComponentRotation());
+}
+
+void UGrabber::Release()
+{
+	UE_LOG(LogTemp, Log, TEXT("Grab Released"));
+}
+
+void UGrabber::Grab()
+{
+	UPhysicsHandleComponent* PhysicsHandler = GetPhysicsHandle();
+	
+	if(PhysicsHandler == nullptr)
+	{
+		return;
+	}
+	
+	const FVector Start = GetComponentLocation();
+	const FVector End = Start + GetForwardVector() * MaxGrabDistance;
+	UWorld* World = GetWorld();
+	DrawDebugLine(World, Start, End, FColor::Red);
+	DrawDebugSphere(World,End,10,10,FColor::Blue, false, 5);
+
+	FCollisionShape Sphere = FCollisionShape::MakeSphere(GrabRadius);
+	FHitResult HitResult;
+	bool IsHit = World->SweepSingleByChannel(
+		HitResult,
+		Start, End,
+		FQuat::Identity,
+		ECC_GameTraceChannel2,
+		Sphere);
+
+	if(IsHit)
+	{
+		DrawDebugSphere(World,HitResult.Location,10,10,FColor::Red, false, 5);
+		DrawDebugSphere(World,HitResult.ImpactPoint,10,10,FColor::Yellow, false, 5);
+		UPrimitiveComponent* PrimitiveComponent =  HitResult.GetComponent();
+		
+		PhysicsHandler->GrabComponentAtLocationWithRotation(
+			PrimitiveComponent,
+			NAME_None,
+			HitResult.ImpactPoint,
+			GetComponentRotation());
+	}
+}
+
+UPhysicsHandleComponent* UGrabber::GetPhysicsHandle() const
+{
+	UPhysicsHandleComponent* PhysicsHandler = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	if(PhysicsHandler == nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PhysicsHandler is nullptr!!!"));
+	}
+	
+	return PhysicsHandler;
+}
+
---
+++ Source/CryptRaider/Grabber.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
+#include "Components/PrimitiveComponent.h"
+#include "Grabber.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class CRYPTRAIDER_API UGrabber : public USceneComponent
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(EditAnywhere)
+	float MaxGrabDistance = 400.0f;
+	UPROPERTY(EditAnywhere)
+	float GrabRadius = 100.0f;
+	UPROPERTY(EditAnywhere)
+	float HoldDistance = 200.0f;
+public:	
+	// Sets default values for this component's properties
+	UGrabber();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	UFUNCTION(BlueprintCallable)
+	void Release();
+	UFUNCTION(BlueprintCallable)
+	void Grab();
+	UPhysicsHandleComponent* GetPhysicsHandle() const;
+};
